4dbe71fe2ef86581a0f864468442778009c695fe
crochetcode <fred.davison@outlook.com>
2021-03-06 22:39:54 -0600

    first draft

---
+++ src/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const Home = () => {
+    return (
+        <div className='page'>
+            <section className='section-left'>
+                <h1>Lentil Cup</h1>
+                <button className='btn-big btn-left'>Enter</button>
+            </section>
+            <section className='section-right'>
+                <h1>Lentil Cup 2</h1> 
+                <button className='btn-big btn-right'>Enter</button>  
+            </section>
+        </div>
+    )
+}
