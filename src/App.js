57bf84ff302ebf0c764eb4a093ddcc54d21ff5f2
fdavison1 <fred.noreply@gmail.com>
2021-03-31 21:38:26 -0500

    wireframe

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import './Reset.css';
+import './style/Maps.css'
 import './App.css';
-import {Switch, Route} from 'react-router-dom';
-import {Split} from './Split';
-import {Sample} from './Sample';
 import {Match} from './Match';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path='/' component={Split}/>
-      <Route exact path='/two' component={Sample}/>
-      <Route path='/two/match' component={Match}/>
-    </Switch>
+    <Match/>
   );
 }
 
