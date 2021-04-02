f721825c07b7b9fdee6538baa35da1edd7f2c7c5
fdavison1 <fred.noreply@gmail.com>
2021-04-01 20:09:38 -0500

    map draft 1

--- src/MapModal.jsx
+++ src/MapModal.jsx
@@ -1,35 +1,43 @@
 import React from 'react';
 
+const maps = ['bog', 'crochetbia', 'ponds', 'arena', 'tideout'];
+
+export const mapNames = {
+    bog: 'BogSocratra',
+    crochetbia: 'Crochetbia',
+    ponds: 'Ponds',
+    arena: 'ReverseArena',
+    tideout: 'Tideout'
+}
+
 export const MapModal = props => {
+    const mapMaps = maps.map(map => {
+        return (
+            <div className='flex flex-col' onClick={() => manageMapArray(map)}>
+                <div className={`map ${map}`}>{props.mapsArray.find(el => el === map) && 'X'}</div>
+                <p>{mapNames[map]}</p>
+            </div>
+        )
+    })
+
+    const manageMapArray = map => {
+        if(props.mapsArray.find(el => el === map)){
+            return 
+        }
+
+        props.addMap([...props.mapsArray, map])       
+    }
+
     return (
         <div className="modal flex">
             <div className="white flex flex-col">
-                <h2>pick one</h2>
+                <h2>{props.info.player} {props.info.message}</h2>
                 <div className='flex'>
-                    <div className='flex flex-col'>
-                        <div className="map bog"></div>
-                        <p>BogSocratra</p>
-                    </div>
-                    <div className='flex flex-col'>
-                        <div className="map crochetbia"></div>
-                        <p>Crochetbia</p>
-                    </div>
-                    <div className='flex flex-col'>
-                        <div className="map ponds"></div>
-                        <p>Ponds</p>
-                    </div>
-                    <div className='flex flex-col'>
-                        <div className="map arena"></div>
-                        <p>Reverse Arena</p>
-                    </div>
-                    <div className='flex flex-col'>
-                        <div className="map tideout"></div>
-                        <p>Tideout</p>
-                    </div>
+                    {mapMaps}
                 </div>
                 <div className="buttons flex">
-                <button onClick={() => props.toggleMaps(false)}>submit</button>
-                <button onClick={() => props.toggleMaps(false)}>close</button>
+                    <button onClick={() => props.toggleMaps(false)}>submit</button>
+                    <button onClick={() => props.toggleMaps(false)}>close</button>
                 </div>
             </div>
         </div>
--- src/Match.jsx
+++ src/Match.jsx
@@ -1,71 +1,121 @@
 import React, { useState } from 'react';
-import {MapModal} from './MapModal';
-import {CivModal} from './CivModal';
+import { MapModal } from './MapModal';
+import { CivModal } from './CivModal';
+import { mapNames } from './MapModal';
 
 export const Match = () => {
     const [showMaps, toggleMaps] = useState(false);
     const [showCivs, toggleCivs] = useState(false);
 
+    const [mapsArray, addMap] = useState([]);
+    const [player1, changePlayer1] = useState('Player 1');
+    const [player2, changePlayer2] = useState('Player 2');
+
+    const [info, setInfo] = useState({
+        player: '',
+        number: '',
+        message: ''
+    });
+
+    const onMapClick = (player, number, message) => {
+        setInfo({
+            player,
+            number,
+            message
+        });
+
+        toggleMaps(true);
+    }
+
+    const returnMap = map => {
+        console.log(mapsArray)
+        return (
+            <div className='flex flex-col'>
+                <div className={`map ${map}`}></div>
+                <p>{mapNames[map]}</p>
+            </div>
+        )
+    }
+
+    const pickOne = (player, number, message, i) => {
+        return (
+            mapsArray[i] ?
+                returnMap(mapsArray[i])
+                : <div className="plus flex" onClick={() => onMapClick(player, number, message)}>1</div>
+        )
+    }
+
+    const pickTwo = (player, number, message, i) => {
+        return (
+            mapsArray[2] ?
+                <>
+                {returnMap(mapsArray[i-1])}
+                {returnMap(mapsArray[i])}
+                </>
+                : <div className="plus flex" onClick={() => onMapClick(player, number, message)}>2</div>
+        )
+    }
+
     return (
         <>
-        {showMaps && <MapModal toggleMaps={toggleMaps}/> }
-        {showCivs && <CivModal toggleMaps={toggleCivs}/>}
-        <div className='page'>
-            <div className="container flex">
-                <div className="player">
-                    <input type="text" placeholder='Player 1' />
-                </div>
-                <div className="player">Best of 3</div>
-                <div className="player">
-                    <input type="text" placeholder='Player 2' className='right' />
-                </div>
-            </div>
-            <br/>
-            <br/>
-            <br/>
-            <h1>Maps</h1>
-            <div className="container flex">
-                <div className="maps flex">
-                    <div className="plus flex" onClick={() => toggleMaps(true)}>1</div>
-                    <div className="plus flex" onClick={() => toggleMaps(true)}>1</div>
-                </div>
-
-                <div className='flex flex-col gray'>
-                    <div className="map megarandom"></div>
-                    <p>Megarandom</p>
-                </div>
-                
-
-                <div className="maps flex">
-                    <div className="plus flex" onClick={() => toggleMaps(true)}>2</div>
-                </div>
-
-            </div>
-            <br/>
-            <br/>
-            <br/>
-            <h1>Civs</h1>
-            <div className="container flex">
-            <div className="maps flex">
-            <div className="plus flex" onClick={() => toggleCivs(true)}>1</div>
-            <div className="plus flex" onClick={() => toggleCivs(true)}>2</div>
-            <div className="plus flex" onClick={() => toggleCivs(true)}>1</div>
-            <div className="plus flex red" onClick={() => toggleCivs(true)}>×</div>
-                </div>
-                <div className="gray">
-                <div className='flex flex-col'>
-                    <div>
-                        <img src='https://static.ageofempires.com/aoe/wp-content/themes/msgpwebteam/dist/images/hero-logo-age2_ea9d88d4.png' alt=""/>
+            {showMaps && <MapModal toggleMaps={toggleMaps} mapsArray={mapsArray} addMap={addMap} info={info} />}
+            {showCivs && <CivModal toggleMaps={toggleCivs} />}
+            <div className='page'>
+                <div className="container flex">
+                    <div className="player">
+                        <input type="text" placeholder='Player 1' onChange={e => changePlayer1(e.target.value)} />
+                    </div>
+                    <div className="player">Best of 3</div>
+                    <div className="player">
+                        <input type="text" placeholder='Player 2' className='right' onChange={e => changePlayer2(e.target.value)} />
                     </div>
                 </div>
+                <br />
+                <br />
+                <br />
+                <h1>Maps</h1>
+                <div className="container flex">
+                    <div className="maps flex">
+                            {pickOne(player1, 1, 'pick one', 3)}
+                            {pickOne(player1, 1, 'pick one', 0)}
+                    </div>
+
+                    <div className='flex flex-col gray'>
+                        <div className="map megarandom"></div>
+                        <p>Megarandom</p>
+                    </div>
+
+
+                    <div className="maps flex">
+                        {pickTwo(player2, 2, 'pick two', 2)}
+                    </div>
+
                 </div>
-                <div className="maps flex">
-                <div className="plus flex red" onClick={() => toggleCivs(true)}>×</div>
-                <div className="plus flex" onClick={() => toggleCivs(true)}>2</div>
-                <div className="plus flex" onClick={() => toggleCivs(true)}>2</div>
+                <br />
+                <br />
+                <br />
+                <h1>Civs</h1>
+                <div className="container flex">
+                    <div className="maps flex">
+                        <div className="plus flex" onClick={() => toggleCivs(true)}>1</div>
+                        <div className="plus flex" onClick={() => toggleCivs(true)}>2</div>
+                        <div className="plus flex" onClick={() => toggleCivs(true)}>1</div>
+                        <div className="plus flex red" onClick={() => toggleCivs(true)}>×</div>
+                    </div>
+                    <div className="gray">
+                        <div className='flex flex-col'>
+                            <div>
+                                <img src='https://static.ageofempires.com/aoe/wp-content/themes/msgpwebteam/dist/images/hero-logo-age2_ea9d88d4.png' alt="" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="maps flex">
+                        <div className="plus flex red" onClick={() => toggleCivs(true)}>×</div>
+                        <div className="plus flex" onClick={() => toggleCivs(true)}>2</div>
+                        <div className="plus flex" onClick={() => toggleCivs(true)}>2</div>
+                    </div>
                 </div>
             </div>
-        </div>
         </>
     )
 }
